fd95271f58484dcdf8c8c3f9e994082cf2b08a7f
codesinging <codesinging@gmail.com>
2022-03-15 10:37:29 +0800

    新增基础命令支持类

---
+++ src/Support/Console/BaseCommand.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace CodeSinging\PinAdmin\Support\Console;
+
+use Illuminate\Console\Command;
+
+class BaseCommand extends Command
+{
+    /**
+     * Write a title string.
+     *
+     * @param string $title
+     * @param string $prefix
+     *
+     * @return void
+     */
+    protected function title(string $title, string $prefix = '- ')
+    {
+        $this->line($prefix . $title);
+    }
+}
